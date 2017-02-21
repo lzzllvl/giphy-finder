bca93e33096e3eba90894797868edec6c49c0adf
Greg Lee <gml2136@gmail.com>
2017-02-20 18:44:34 -0800

    just getting started, haven't even done the ajax calls yet

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -2,6 +2,16 @@ let initialChoices = ["Tupac", "Chance the Rapper", "Andre 3000", "Big Boi", "Bi
 
 let createButtons = (currentValue) => {
   let btn = $("<button>");
-  btn.text(currentValue); // need to find a way to add a space
+  btn.text(currentValue);
+  btn.attr("data-name", currentValue);
   btn.appendTo($("#btn-list")).after("\u00A0 \u00A0");
+};
+
+let apiKey = "dc6zaTOxFJmzC";
+let urlBase = "http://api.giphy.com/v1/gifs/search";
+let paramObj = {
+  q: "",
+  limit: 10,
+  rating: "pg",
+  api_key: apiKey,
 };
